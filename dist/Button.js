86ef7c764ffef6f4361e43cc228f6445f39dd8e3
jacquesduv <31418469+jacquesduv@users.noreply.github.com>
2024-03-05 16:55:44 +0200

    Updated babel.

--- dist/Button.js
+++ dist/Button.js
@@ -1,13 +1,21 @@
-import React from 'react';
-const Button = ({
-  children,
-  onClick
-}) => /*#__PURE__*/React.createElement("button", {
-  onClick: onClick,
-  style: {
-    padding: '10px',
-    backgroundColor: 'blue',
-    color: 'white'
-  }
-}, children);
-export default Button;
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+var Button = function Button(_ref) {
+  var children = _ref.children,
+    onClick = _ref.onClick;
+  return /*#__PURE__*/_react["default"].createElement("button", {
+    onClick: onClick,
+    style: {
+      padding: '10px',
+      backgroundColor: 'blue',
+      color: 'white'
+    }
+  }, children);
+};
+var _default = exports["default"] = Button;
