c62ac51d7f049f9c41af477b540ca1d8499b64e9
jacquesduv <31418469+jacquesduv@users.noreply.github.com>
2024-03-06 14:22:05 +0200

    New ts component setup.

--- src/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Button = function({ children, onClick }) { return (
-  <button onClick={onClick} style={{ padding: '10px', backgroundColor: 'blue', color: 'white' }}>
-    {children}
-  </button>
-) };
-
-export default Button;
